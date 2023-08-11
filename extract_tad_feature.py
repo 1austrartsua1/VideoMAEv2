0edd6b0c1a1b73b4745a34c14d1cb19c42f855e9
1austrartsua1 <patrick.r.johnstone@gmail.com>
2023-08-11 14:42:44 -0400

    modifications to extract_tad_feature.py Fixing author username.

--- extract_tad_feature.py
+++ extract_tad_feature.py
@@ -1,4 +1,6 @@
 """Extract features for temporal action detection datasets"""
+import time 
+
 import argparse
 import os
 import random
@@ -7,6 +9,8 @@ import numpy as np
 import torch
 from timm.models import create_model
 from torchvision import transforms
+
+import tqdm 
 
 # NOTE: Do not comment `import models`, it is used to register models
 import models  # noqa: F401
@@ -82,6 +86,10 @@ def get_args():
         '--ckpt_path',
         default='YOUR_PATH/vit_g_hyrbid_pt_1200e_k710_ft.pth',
         help='load from checkpoint')
+    parser.add_argument(
+        '--device',
+        default=0,
+        help='GPU device') 
 
     return parser.parse_args()
 
@@ -89,7 +97,10 @@ def get_args():
 def get_start_idx_range(data_set):
 
     def thumos14_range(num_frames):
-        return range(0, num_frames - 15, 4)
+        # PJ this is where you select the feature stride
+        # set to 4 for thumos14.
+        # we can adjust this if we want. 
+        return range(0, num_frames - 15, 4) 
 
     def fineaction_range(num_frames):
         return range(0, num_frames - 15, 16)
@@ -117,6 +128,7 @@ def extract_feature(args):
     random.shuffle(vid_list)
 
     # get model & load ckpt
+    print('loading model to GPU...')
     model = create_model(
         args.model,
         img_size=224,
@@ -138,23 +150,46 @@ def extract_feature(args):
     # extract feature
     num_videos = len(vid_list)
     for idx, vid_name in enumerate(vid_list):
+        print(idx,vid_name)
         url = os.path.join(args.save_path, vid_name.split('.')[0] + '.npy')
         if os.path.exists(url):
+            print('file already exists, continuing...')
             continue
+        
+        # create an empty file place-holder to stop other processes from trying to process this video
+        with open(url, 'w') as fp:
+            pass
 
         video_path = os.path.join(args.data_path, vid_name)
         vr = video_loader(video_path)
 
+        
         feature_list = []
         for start_idx in start_idx_range(len(vr)):
+            print(start_idx,'out of',start_idx_range(len(vr)))
             data = vr.get_batch(np.arange(start_idx, start_idx + 16)).asnumpy()
+            # PJ: numframes is cleary 16 based on above np.arange call
             frame = torch.from_numpy(data)  # torch.Size([16, 566, 320, 3])
             frame_q = transform(frame)  # torch.Size([3, 16, 224, 224])
             input_data = frame_q.unsqueeze(0).cuda()
-
             with torch.no_grad():
                 feature = model.forward_features(input_data)
                 feature_list.append(feature.cpu().numpy())
+
+        if len(feature_list) == 0:
+            #pj: the video was too short and no feature were computed with
+            #the given stride and window. 
+            #this can happen when you downsample a video aggressively.
+            #If this happens it may be a problem for training/eval-ing actionFormer
+            #since we do not have features for this video but it will show up
+            #in the annotations json file and I'm not sure how actionFormer reacts 
+            #in this case. My current strategy is when this happens due to 
+            #aggressive downsampling, rerun at high enough downsampling FPS so this
+            #will not happen. For Thumos, this happens with fps=1, but should not at fps=2. 
+            #this is because we need at least 20 frames to avoid the problem. 
+
+            print(video_path,'is too short so no features extracted, ignoring this video')
+            continue 
 
         # [N, C]
         np.save(url, np.vstack(feature_list))
@@ -162,5 +197,9 @@ def extract_feature(args):
 
 
 if __name__ == '__main__':
+    t = time.time()
     args = get_args()
+    torch.cuda.set_device(int(args.device))
     extract_feature(args)
+    print('running time to extract features:',time.time()-t)
+    
